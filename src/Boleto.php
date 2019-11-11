ed8e3e297f46158b2e3a84452e152840b638fa35
amadeomendez <amadeo.mendez@outlook.com>
2019-11-10 22:59:23 -0300

    Elimina elementos innecesarios de la clase Boleto

--- src/Boleto.php
+++ src/Boleto.php
@@ -12,31 +12,11 @@ class Boleto implements BoletoInterface {
 
     protected $fecha;
 
-    protected $descripcion;
-
-    public function __construct($valor, $colectivo, $tarjeta, $fecha, $tipoPago) {
+    public function __construct($valor, $colectivo, $tarjeta, $fecha) {
         $this->valor = $valor;
         $this->colectivo = $colectivo;
         $this->tarjeta = $tarjeta;
         $this->fecha = $fecha;
-        switch ($tipoPago) {
-            case "un plus":
-                $this->descripcion = "Abona viaje plus ".$valor." y";
-                break;
-            case "dos plus":
-                $this->descripcion = "Abona viajes plus ".($valor*2)." y";
-                break;
-            case "transbordo y un plus":
-                $this->descripcion = "Abona viaje plus ".($valor*3)." y";
-                break;
-            case "transbordo y dos plus":
-                $this->descripcion = "Abona viajes plus ".($valor*6)." y";
-                break;
-        }
-    }
-
-    public function obtenerDescripcion() {
-        return $this->descripcion;
     }
 
     public function obtenerValor() {
--- src/BoletoInterface.php
+++ src/BoletoInterface.php
@@ -4,15 +4,13 @@ namespace TrabajoTarjeta;
 
 interface BoletoInterface {
 
-    public function obtenerDescripcion();
-
     /**
      * Devuelve el valor del boleto.
      *
      * @return int
      */
     public function obtenerValor();
-    
+
     /**
      * Devuelve la tarjeta con la que se pago.
      *
@@ -22,17 +20,17 @@ interface BoletoInterface {
     /**
      * Devuelve la fecha en la que se imprimió el boleto.
      *
-     * @return 
+     * @return
      */
     public function obtenerFecha();
-    
+
     /**
      * Devuelve el tipo de trajeta con la que se pago.
      *
-     * @return 
+     * @return
      */
     public function obtenerTipoTarjeta();
-    
+
     /**
      * Devuelve un objeto que respresenta el colectivo donde se viajó.
      *
