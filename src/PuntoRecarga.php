001299175502bed60a8a333d6ec0cb6424eb2c56
amadeomendez <amadeo.mendez@outlook.com>
2019-11-10 22:56:20 -0300

    Completa la clase de punto de recarga

--- src/PuntoRecarga.php
+++ src/PuntoRecarga.php
@@ -5,23 +5,21 @@ namespace TrabajoTarjeta;
 class PuntoRecarga implements PuntoRecargaInterface{
 
   public function recarga(TarjetaInterface $tarjeta, $monto) {
-    //Chequea si es alguno de los valores aceptados que no cargan dinero extra
+    // Chequea si es alguno de los valores aceptados que no cargan dinero extra
     if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100) {
-      $tarjeta->saldo += $monto;
+      $tarjeta->sumarSaldo($monto);
       return true;
     }
-    //Chequea si es alguno de los que sí cargan extra
-    if ($monto == 510.15) {
-      $this->saldo += $monto + 81.93 - ($this->plus * $this->precio);
-      $this->plus = 0;
+    // Chequea si es alguno de los que sí cargan extra
+    if ($monto == 1119,90) {
+      $this->sumarSaldo($monto + 180,10);
       return true;
     }
-    if ($monto == 962.59) {
-      $this->saldo += $monto + 221.58 - ($this->plus * $this->precio);
-      $this->plus = 0;
+    if ($monto == 2114,11) {
+      $this->sumarSaldo($monto + 485,89);
       return true;
     }
-    //Si no es ninguno de esos valores entonces no es un valor aceptado y hay que retornar false
+    // Si no es ninguno de esos valores entonces no es un valor aceptado y hay que retornar false
     return false;
   }
 
